82cf927cedfc163ef51c34617ded468cb36ddf09
CrosX <crosx1019@gmail.com>
2021-08-05 07:43:19 +0800

    add module_home

--- app/src/main/kotlin/com/crosx/kotlin/MainActivity.kt
+++ app/src/main/kotlin/com/crosx/kotlin/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.crosx.kotlin
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
 import com.crosx.common.base.BaseActivity
+import com.crosx.module_home.TestActivity
 
 class MainActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val text = findViewById<TextView>(R.id.test)
+        text.setOnClickListener {
+            startActivity(Intent(this, TestActivity::class.java))
+        }
     }
 }
